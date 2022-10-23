085b7f849a0c7e6301ed46728cd12089539695e3
GLABBER <serjglabber@gmail.com>
2022-10-23 19:58:29 +0300

    Simplified code

--- CoreDataDemo/Model/CoreDataManager.swift
+++ CoreDataDemo/Model/CoreDataManager.swift
@@ -1,37 +1,27 @@
 //
-//  AppDelegate.swift
+//  CoreDataManager.swift
 //  CoreDataDemo
 //
-//  Created by GLABBER on 22.10.2022.
+//  Created by GLABBER on 23.10.2022.
 //
 
-import UIKit
+import Foundation
 import CoreData
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    
+    private init() {}
+    
+    lazy var context: NSManagedObjectContext = {
+        persistentContainer.viewContext
+    }()
+    
+    // Описание сущности
+    func entityForName(entityName: String) -> NSEntityDescription {
+        return NSEntityDescription.entity(forEntityName: entityName, in: context)!
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
+    
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
@@ -78,4 +68,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -13,61 +13,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Ссылка на AppDelegate
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
-        // Создаем контекст
-        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
-        
-        // Описание сущности
-        let entityDescription = NSEntityDescription.entity(forEntityName: "Person", in: context)
-        
         // Создание объекта
-        //let managedObject = NSManagedObject(entity: entityDescription!, insertInto: context)
-        let managedObject = Person(entity: entityDescription!, insertInto: context)
+        let managedObject = Person()
         
         // Установка значений атрибутов
-//        managedObject.setValue("Вася", forKey: "name")
-//        managedObject.setValue(18, forKey: "age")
-        managedObject.name = "Оля"
-        managedObject.age = 34
+        managedObject.name = "Петя"
+        managedObject.age = 28
         
         // Извлекаем значение атрибута
-//        let name = managedObject.value(forKey: "name")
-//        let age = managedObject.value(forKey: "age")
         let name = managedObject.name
         let age = managedObject.age
         
         print("\(name), \(age)")
         
         // Сохранение данных
-        appDelegate.saveContext()
+        CoreDataManager.instance.saveContext()
         
         // Извлекаем данные и базы данных
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Person")
         do {
-            let results = try context.fetch(fetchRequest)
-//            for result in results as! [NSManagedObject]{
-//                print("name - \(result.value(forKey: "name")!), age - \(result.value(forKey: "age")!)")
-//            }
+            let results = try CoreDataManager.instance.context.fetch(fetchRequest)
             for result in results as! [Person] {
                 print("name - \(result.name), age - \(result.age)")
-        }
-        } catch {
-            print(error)
-        }
-        
-        // Удаление ВСЕХ записей
-        do {
-            let results = try context.fetch(fetchRequest)
-            for result in results as! [NSManagedObject]{
-                context.delete(result)
             }
         } catch {
             print(error)
         }
         
-        // Сохраняем
-        appDelegate.saveContext()
+        // Удаление ВСЕХ записей
+//        do {
+//            let results = try CoreDataManager.instance.context.fetch(fetchRequest)
+//            for result in results as! [NSManagedObject]{
+//                CoreDataManager.instance.context.delete(result)
+//            }
+//        } catch {
+//            print(error)
+//        }
+//
+//        // Сохраняем
+//        CoreDataManager.instance.saveContext()
     }
 }
